b5be273c1757150dbeb9b58b9a2b82bd3eb388a3
Sun <56666749+feelingphs@users.noreply.github.com>
2021-10-13 13:37:45 +0900

    tasoo

---
+++ work01/js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	$(window).scroll(function(){
+		$('.tasoo div').each(function(){
+			var tasoo_element = $(this).offset().top + $(this).outerHeight();
+			var tasoo_window = $(window).scrollTop() + $(window).height();
+
+			if( tasoo_window > tasoo_element ){
+				$(this).animate({'opacity':'1','margin-bottom':'0px'},300);
+			}
+		}); //tasoo
+
+		$('.strong section').each(function(){
+			var strong_element = $(this).offset().top + $(this).outerHeight();
+			var strong_window = $(window).scrollTop() + $(window).height();
+
+			if( strong_window > strong_element ){
+				$(this).animate({'opacity':'1','margin-bottom':'0px'},300);
+			}
+		}); //strong
+	}); //window
+});

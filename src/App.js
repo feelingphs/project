fad14046b8f390058acbb767f80e837fff7ac1bc
박혜선 <hspark@vinylc.com>
2024-10-25 17:58:00 +0900

    틱택토 수정중

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import { useState } from 'react';
 
-function Square({ value }) {
+function Square() {
 	const [value, setValue] = useState(null);
 	
 	function handleClick(){
-		console.log("dd");
+		setValue('X');
 	}
 	return <button className="square" onClick={handleClick}>{ value }</button>
 }
